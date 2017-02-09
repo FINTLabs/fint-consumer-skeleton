c6bde6004b8eadd612c0f2b535d3b7f246e90a47
jarlehansen <jarle@jarlehansen.net>
2017-02-09 22:58:16 +0100

    added nodejs client to test health check

---
+++ test-clients/health.js
@@ -0,0 +1,43 @@
+const uuid = require('uuid')
+const amqp = require('amqplib')
+
+console.log('Usage: npm run health <orgId>')
+
+const config = {
+  host: 'localhost',
+  user: 'guest',
+  password: 'guest',
+  vhost: '',
+  orgId: process.argv[2] === undefined ? 'rogfk.no' : process.argv[2]
+}
+
+const json = JSON.stringify({
+  corrId: uuid(),
+  action: 'HEALTH_CHECK',
+  status: "PROVIDER_ACCEPTED",
+  time: new Date().getTime(),
+  orgId: config.orgId,
+  source: "fk",
+  client: "vfs",
+  message: null,
+  data: []
+})
+ 
+const connectionString = `amqp://${config.user}:${config.password}@${config.host}:5672/${config.vhost}`
+const queue = `${config.orgId}.downstream`
+
+const consumeMsg = (channel) => {
+  return channel.assertQueue(queue, {durable: true}).then((ok) => {
+    return channel.consume(queue, (msg) => {
+      channel.publish('', msg.properties.replyTo, new Buffer(json), { 'contentType': 'application/json' })
+      channel.ack(msg)
+    })
+  })
+}
+
+const connection = amqp.connect(connectionString)
+connection.then((con) => {
+  return con.createChannel()
+}).then((channel) => {
+  return consumeMsg(channel)
+}).catch(console.warn)
