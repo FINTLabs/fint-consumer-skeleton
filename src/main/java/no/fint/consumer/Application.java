cb353c6966a23378f95dbe35d66e31410fe19ca8
jarlehansen <jarle@jarlehansen.net>
2017-04-14 20:42:33 +0200

    updated fint library and gradle versions

--- src/main/java/no/fint/consumer/Application.java
+++ src/main/java/no/fint/consumer/Application.java
@@ -2,11 +2,13 @@ package no.fint.consumer;
 
 import com.github.springfox.loader.EnableSpringfox;
 import no.fint.audit.EnableFintAudit;
+import no.fint.dependencies.annotations.EnableFintDependencies;
 import no.fint.events.EnableFintEvents;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableFintDependencies
 @EnableFintEvents
 @EnableFintAudit
 @EnableScheduling
