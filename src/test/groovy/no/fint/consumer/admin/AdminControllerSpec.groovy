5839759a05400f3dee3372846618920d53d6abbe
Asgeir Storesund Nilsen <asgeir@fintlabs.no>
2020-10-07 08:23:55 +0200

    Use latest fint-events (#31)
    * Use latest fint-events
    
    Without health check feature.
    
    * Updated implementation for new fint-events.
    
    * Refactor for clarity.
    
    * Use release 2.3.0 of fint-events

--- src/test/groovy/no/fint/consumer/admin/AdminControllerSpec.groovy
+++ src/test/groovy/no/fint/consumer/admin/AdminControllerSpec.groovy
@@ -1,10 +1,12 @@
 package no.fint.consumer.admin
 
+import no.fint.audit.FintAuditService
 import no.fint.consumer.event.ConsumerEventUtil
 import no.fint.consumer.event.SynchronousEvents
 import no.fint.event.model.DefaultActions
 import no.fint.event.model.Event
 import no.fint.event.model.HeaderConstants
+import no.fint.events.FintEvents
 import no.fint.test.utils.MockMvcSpecification
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -16,13 +18,18 @@ class AdminControllerSpec extends MockMvcSpecification {
     private MockMvc mockMvc
     private SynchronousEvents synchronousEvents
     private BlockingQueue queue
+    private FintEvents fintEvents
 
     void setup() {
         synchronousEvents = Mock()
         queue = Mock()
+        fintEvents = Mock()
         healthController = new AdminController(
-                synchronousEvents: synchronousEvents,
-                consumerEventUtil: Mock(ConsumerEventUtil)
+                consumerEventUtil: new ConsumerEventUtil(
+                        synchronousEvents: synchronousEvents,
+                        fintEvents: fintEvents,
+                        fintAuditService: Mock(FintAuditService)
+                )
         )
         mockMvc = MockMvcBuilders.standaloneSetup(healthController).build()
     }
@@ -32,6 +39,7 @@ class AdminControllerSpec extends MockMvcSpecification {
         def response = mockMvc.perform(get("/admin/health").header(HeaderConstants.ORG_ID, "mock.no").header(HeaderConstants.CLIENT, "mock"))
 
         then:
+        1 * fintEvents.sendDownstream(_ as Event) >> true
         1 * synchronousEvents.register(_ as Event) >> queue
         1 * queue.poll(_, _) >> new Event(action: DefaultActions.HEALTH.name())
         response.andExpect(status().isOk())
